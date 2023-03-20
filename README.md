6d451b5df674ce10eb985d99d5629e55d8d5f7ca
cristino4 <117871733+cristino4@users.noreply.github.com>
2023-03-19 18:27:02 -0700

    added function to create license badge

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# 9-professional-readme-generator
-A command-line application that dynamically generates a professional README.md file from a user's input
+# 9-Professional-README-GENERATOR
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+
+- [9-Professional-README-GENERATOR](#9-professional-readme-generator)
+  - [Table of Contents](#table-of-contents)
+  - [License](#license)
+  - [Installation Instructions](#installation-instructions)
+  - [How to Contribute](#how-to-contribute)
+  - [Description](#description)
+  - [Questions](#questions)
+  - [Test Instructions](#test-instructions)
+  - [Usage Information](#usage-information)
+## License
+
+MIT License
+## Installation Instructions
+
+To use this project, just run index.js using NODE.js. Make sure to install NODE.js here: https://nodejs.org/en
+## How to Contribute
+
+To contribute, please send an inquiry and request to contribute to my contact email below.
+
+## Description
+
+Repository Link: [www.github.com/cristino4](https://github.com/cristino4/9-professional-readme-generator)
+This project is a script that helps create a README file. Just answer the questions provided and the README file will be created.
+## Questions
+
+Questions may be sent to:
+* Email: cristinocastro4@gmail.com
+* GitHub Username: [cristino4](https://github.com/cristino4)
+## Test Instructions
+
+To test the project, download all the source files. Then run index.js using node (node index.js) in a console.
+## Usage Information
+
+To use the project, run index.js. Then answer the questions prompted.
--- index.js
+++ index.js
@@ -1,19 +1,75 @@
 const inquirer = require('inquirer');
+const fs = require('fs')
+const writer = require('writer')
 
-console.log("Welcom the the professional README generator!");
+const licenseList = ['Apache License 2.0', 'GNU General Public LIcense v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License',
+'BSD 3-clause "New" of "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0',
+'GNU General Public License v2.0', 'GNU Lesser General Public License v3.0', 'Mozilla Public License 2.0',
+'The Unlicense'];
+
+const licenseBadges = [
+  {
+    license: "Apache License 2.0",
+    badge: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  },
+  {
+    license: "GNU General Public LIcense v3.0",
+    badge: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  },
+  {
+    license: "MIT License",
+    badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  },
+  {
+    license: 'BSD 2-Clause "Simplified" License',
+    badge: "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
+  },
+  {
+    license: 'BSD 3-clause "New" of "Revised" License',
+    badge: "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  },
+  {
+    license: "Boost Software License 1.0",
+    badge: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  },
+  {
+    license: "Creative Commons Zero v1.0 Universal",
+    badge: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  },
+  {
+    license: "Eclipse Public License 2.0",
+    badge: "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
+  },
+  {
+    license: "GNU General Public License v2.0",
+    badge: "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+  },
+  {
+    license: "GNU Lesser General Public License v2.1",
+    badge: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)"
+  },
+  {
+    license: "Mozilla Public License 2.0",
+    badge: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  },
+  {
+    license: "The Unlicense",
+    badge: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  }
+]
+
+console.log("Welcome the the professional README generator!");
 
 const questions  = [
   {
     type: 'input',
+    name: 'repository',
+    message: 'Enter the project GitHub repository link'
+  },
+  {
+    type: 'input',
     name: 'projectTitle',
     message: 'What is the project title?',
-    // validate(value){
-    //   const pass = typeof value;
-    //   if (pass = 'string'){
-    //     return true;
-    //   } 
-    //   return 'please enter a valid project title';
-    // },
   },
   {
     type: 'input',
@@ -42,9 +98,9 @@ const questions  = [
   },
   {
     type: 'list',
-    name: 'licenses',
+    name: 'license',
     message: 'Enter the license for the project',
-    choices: ['MIT License', 'Other1', 'other2']
+    choices: licenseList
   },
   {
     type: 'input',
@@ -58,10 +114,95 @@ const questions  = [
   }
 ];
 
+function parseBadge(answers){
+  license = answers.license;
+  for (i = 0; i<licenseBadges.length; i++){
+    if(licenseBadges[i].license === license){
+      return licenseBadges[i].badge
+    }
+  }
+}
+
+function updateReadme(answers){
+  console.log(answers)
+  //title
+  title = `# ${answers.projectTitle}\n`
+  fs.writeFile('readme1.md', title, function (err) {
+    if (err) throw err;
+  })
+    //license badge
+    badge = parseBadge(answers)
+    msg = `\n${badge}\n`
+    fs.appendFile('readme1.md',msg,function(err){
+      if(err) throw err;
+    })
+  //description
+  head = `\n## Description\n`
+  body= `Repository Link: ${answers.repository} \n${answers.projectDescription}\n`
+  message = head + "\n" + body
+  fs.appendFile('readme1.md', message, function (err) {
+    if (err) throw err;
+  })
+  //table of contents
+  head = `\n## Table of Contents\n`
+  body = `* [Description](#description)\n* [Installation Instructions](#installation-instructions)\n* 
+  [Usage Information](#usage-information)\n* [License](#license)\n* [Contributing](#how-to-contribute)\n* [Test](#test-instructions)\n* [Questions](#questions)\n`
+  message = head + "\n" + body
+  fs.appendFile('readme1.md', message, function (err) {
+    if (err) throw err;
+  })
+  //installation
+  head = `## Installation Instructions\n`
+  body = `${answers.installationInstructions}\n`
+  message = head + "\n" + body
+  fs.appendFile('readme1.md', message, function (err) {
+    if (err) throw err;
+  })
+  //usage
+  head = `## Usage Information\n`
+  body = `${answers.usageInformation}\n`
+  message = head + "\n" + body
+  fs.appendFile('readme1.md', message, function (err) {
+    if (err) throw err;
+  })
+  //license
+  head = `## License\n`
+  body = `${answers.license}\n`
+  message = head + "\n" + body
+  fs.appendFile('readme1.md', message, function (err) {
+    if (err) throw err;
+  })
+  //contributing
+  head = `## How to Contribute\n`
+  body = `${answers.contributionGuidelines}\n`
+  message = head + "\n" + body
+  fs.appendFile('readme1.md', message, function (err) {
+    if (err) throw err;
+  })
+  //tests
+  head = `## Test Instructions\n`
+  body = `${answers.testInstructions}\n`
+  message = head + "\n" + body
+  fs.appendFile('readme1.md', message, function (err) {
+    if (err) throw err;
+  })
+  //questions
+  link = `[${answers.githubUsername}](https://github.com/${answers.githubUsername})`
+  head = `## Questions\n`
+  body = `Questions may be sent to:\n* Email: ${answers.email}\n* GitHub Username: ${link}\n`
+  message = head + "\n" + body
+  fs.appendFile('readme1.md', message, function (err) {
+    if (err) throw err;
+  })
+  console.log('File has been saved!');
+
+
+}
 
 inquirer.prompt(questions).then((answers) => {
   console.log('\nOrder receipt:');
-  console.log(JSON.stringify(answers, null, '  '));
+  // ans = JSON.stringify(answers, null, '  ');
+  updateReadme(answers)
   // console.log(answers);
   // console.log(typeof answers)
 });
